c4fd027f81fcfe90051bdd2f9099afc0aa45fda9
yamunaa122 <144665447+yamunaa122@users.noreply.github.com>
2023-09-22 14:40:56 +0530

    Add files via upload

---
+++ unit 2 challanges/bank.py
@@ -0,0 +1,46 @@
+class BankAccount:
+ 
+   def _init_(self, account_number, account_holder_name, initial_balance=0.0):
+ def_init(self,account_number,
+     
+   self.__account_number = account_number
+  
+    self.__account_holder_name = account_holder_name
+   
+     self.__account_balance = initial_bal
+      print("Invalid deposit amount. Please enter a positive amount.")
+
+ 
+   def withdraw(self, amount):
+    
+    if amount > 0 and amount <= self.__account_balance:
+    
+        self.__account_balance -= amount
+       
+     print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+       
+ else:
+            print("Invalid withdrawal amount or insufficient balance.")
+
+ 
+   def display_balance(self):
+  
+      print(f"Account Number: {self.__account_number}")
+  
+      print(f"Account Holder: {self.__account_holder_name}")
+      
+  print(f"Account Balance: ${self.__account_balance}")
+
+# Example usage:
+if _name_ == "__main__":
+  
+  account = BankAccount("123456789", "John Doe", 1000.0)
+    
+
+    account.display_balance()
+ 
+   account.deposit(500.0)
+ 
+   account.withdraw(200.0)
+  
+  account.display_balance():
---
+++ unit 2 challanges/player.py
@@ -0,0 +1,33 @@
+class Player:
+  
+  def play(self):
+  
+      print("The player is playing cricket.")
+
+
+class Batsman(Player):
+   
+ def play(self):
+  
+      print("The batsman is batting.")
+
+
+class Bowler(Player):
+  
+  def play(self):
+   
+     print("The bowler is bowling.")
+
+
+# Example usage:
+
+if _name_ == "__main__":
+  
+  batsman = Batsman()
+  
+  bowler = Bowler()
+   
+ 
+    batsman.play()
+  
+  bowler.play()
